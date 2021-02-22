772137afb598efbb5dea415fa0f7c5da5bd4b7e6
Rishab <rishabsharma120720@gmail.com>
2021-02-22 17:32:10 +0530

    Title updated

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Rishab Sharma";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Blogging Website";
 
 export default function Layout({ children, home }) {
 	return (
